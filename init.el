274dacef991f3a1120926f5b2474cd11ac8fbe6d
Hector93 <hmanuelg93@gmail.com>
2019-06-19 00:04:02 -0500

    añado plugins

--- init.el
+++ init.el
@@ -33,9 +33,3 @@ There are two things you can do about this warning:
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (markdown-mode markdown-mode+ use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
